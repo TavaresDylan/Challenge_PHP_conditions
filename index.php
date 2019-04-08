b9f2d7d7f3e7e5fa65e4bd3dda53788ad6fdc184
TavaresDylan <tavares-dylan03@gmail.com>
2019-04-08 12:28:17 +0200

    Exercice 3

--- index.php
+++ index.php
@@ -1,7 +1,28 @@
 <?php
-//Exercice 1 
-$age = 66;
+//Exercice 1 ##Exercice 1 Créer une variable **age** et l'initialiser avec une valeur.Si l'age est supérieur ou égale à 18, afficher **Vous êtes majeur**. Dans le cas contraire, afficher **Vous êtes mineur**. 
+$age = 11;
 if ($age >= 18 ) {
-	echo "Vous êtes majeur <br />"
-	
+	echo "Vous êtes majeur <br />";
+}
+else{
+	echo "Vous êtes mineur <br />";
+}
+
+//Exercice 2 Créer une variable **IsEasy** de type booléan et l'initialiser avec une valeur.Afficher **C'est facile!!** si c'est vrai. Dans le cas contraire afficher **C'est difficile !!!**.**Bonus :** L'écrire de deux manières différentes.
+$IsEasy = true;
+if ($IsEasy = true) {
+	echo "C'est facile !! <br />";
+}
+else{
+	echo "C'est difficile !! <br />";
+}
+
+//Exercice 3 Créer deux variables **age** et **genre**. La variable **genre** peut prendre comme valeur : - Homme - Femme.
+$age1 = "2";
+$genre = "Femme";
+
+if ($genre == 'Homme') {
+	echo "vous êtes un homme et";
+} else{
+
 }
